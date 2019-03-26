dda884a66f417c402ac01c9bc3280381adede322
Marcin Bień <marcin@bien.pl>
2019-03-26 20:50:33 +0100

    Sample node + express + typescript app

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import {WelcomeController} from './controllers/welcome.controller';
+
+const app: express.Application = express();
+const port: number =  3000;
+
+app.use('/welcome', WelcomeController);
+
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+})
