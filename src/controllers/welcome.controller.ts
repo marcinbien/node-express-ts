fcf277a9c5c0578ab0b4c4bf74a61ca9a453211d
Marcin Bień <marcin@bien.pl>
2019-03-26 21:40:48 +0100

    Added tslint.

--- src/controllers/welcome.controller.ts
+++ src/controllers/welcome.controller.ts
@@ -1,16 +1,15 @@
-import {Router, Request, Response} from 'express';
+import {Router, Request, Response} from "express";
 
 const router: Router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+router.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
 });
 
-router.get('/:name', (req: Request, res: Response) => {
+router.get("/:name", (req: Request, res: Response) => {
     const {name} = req.params;
 
     res.send(`Hello, ${name}!`);
 });
-
 
 export const WelcomeController: Router = router;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express from 'express';
+import express from "express";
 
-import {WelcomeController} from './controllers/welcome.controller';
+import {WelcomeController} from "./controllers/welcome.controller";
 
 const app: express.Application = express();
 const port: number =  3000;
 
-app.use('/welcome', WelcomeController);
-
+app.use("/welcome", WelcomeController);
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
-})
+});
